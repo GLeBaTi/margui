7d1ea5a82f665fb1db071e31c4ea239fa98075f7
Глебов Евгений <gglebati@gmail.com>
2019-09-17 16:11:48 +0300

    stage

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"runtime"
+
+	"github.com/go-gl/glfw/v3.2/glfw"
 )
 
+func init() {
+	// This is needed to arrange that main() runs on main thread.
+	// See documentation for functions that are only allowed to be called from the main thread.
+	runtime.LockOSThread()
+}
+
 func main() {
-	fmt.Println("sdfsdf")
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
+
+	window, err := glfw.CreateWindow(640, 480, "Testing", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+
+	for !window.ShouldClose() {
+		// Do OpenGL stuff.
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
 }
